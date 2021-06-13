5dcae0abfb373d77db15e9646af409f85bfce986
Mohd.Saqib <mohd.saqib@pitechniques.com>
2021-06-13 22:34:22 +0530

    Add project files.

---
+++ CollectionAssignment/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionAssignment
+{
+    public class Customer
+    {
+		// CustomerName, City, Country
+		private string customerName;
+
+		public string CustomerName
+		{
+			get { return customerName; }
+			set { customerName = value; }
+		}
+		private string city;
+
+		public string City
+		{
+			get { return city; }
+			set { city = value; }
+		}
+		private string country;
+
+
+		public string Country
+		{
+			get { return country; }
+			set { country = value; }
+		}
+		#region ctor
+		public Customer(string pcustomerName, string pcity, string pcountry)
+		{
+			this.CustomerName = pcustomerName;
+			this.City = pcity;
+			this.Country = pcountry;
+		}
+		#endregion
+		public override string ToString()
+		{
+			return string.Format($"{CustomerName}, {City}, {Country}");
+		}
+
+	}
+
+
+}
---
+++ CollectionAssignment/CustomerCollection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionAssignment
+{
+    class CustomerCollection
+    {
+        static List<Customer> customerList = new List<Customer>();
+
+        static List<Customer> GetList()
+        {
+            return customerList;
+        }
+        static Customer GetCustomerByName(string pname)
+        {
+            for (int i = 0; i < customerList.Count; i++)
+            {
+                if(customerList[i].CustomerName == pname)
+                {
+                    return customerList[i];
+                }
+            }
+            return null;
+        }
+        static Customer GetCustomerByCitynCountry(string pcity, string pcountry)
+        {
+            for (int i = 0; i < customerList.Count; i++)
+            {
+                if(customerList[i].City == pcity && customerList[i].Country == pcountry)
+                {
+                    return customerList[i];
+                }
+            }
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            customerList.Add(new Customer("Ross", "New York", "USA"));
+            customerList.Add(new Customer("Django", "Chickasaw County", "Arizona"));
+            customerList.Add(new Customer("Chandler", "New York", "USA"));
+            customerList.Add(new Customer("John", "Melbourne", "Australia"));
+            customerList.Add(new Customer("Rachel", "New York", "USA"));
+            customerList.Add(new Customer("Rahul", "Mumbai", "India"));
+            customerList.Add(new Customer("Monica", "New York", "USA"));
+            customerList.Add(new Customer("Brooks", "Zurich", "Switzerland"));
+            customerList.Add(new Customer("Joey", "New York", "USA"));
+
+            Console.WriteLine(GetCustomerByName("Joey"));
+            Console.WriteLine(GetCustomerByCitynCountry("Mumbai", "India"));
+        }
+    }
+}
